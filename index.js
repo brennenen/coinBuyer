d6199745f3442388a0c744f6787d7650e29c4fc8
brennenen <brennen.milam@gmail.com>
2019-11-28 16:48:42 -0500

    getting payment account

--- index.js
+++ index.js
@@ -19,6 +19,35 @@ const authedClient = new CoinbasePro.AuthenticatedClient(
   apiURI
 );
 
+let usdAccount = null;
+let btcAccount = null;
+let paymentAccount = null;
+
+function fundAccount() {
+    authedClient.getPaymentMethods((error, response, data) => {
+      if (error) {
+        // handle the error
+        console.log(error);
+      } else {
+          var accounts = data;
+         
+          //find the usd account
+          var paymentAccounts = accounts.filter(
+            x => x.currency == "USD");
+          if (paymentAccounts != null && paymentAccounts.length > 0) {
+              //lets use the first one
+              this.paymentAccount = paymentAccounts[0];           
+              console.log(`${this.paymentAccount.name} payment account found`);
+          }
+          else{
+              console.log('No payment accounts found');
+              return;
+          }
+      }
+    });
+    return;
+  }
+
 authedClient.getCoinbaseAccounts((error, response, data) => {
   if (error) {
     // handle the error
@@ -26,13 +55,14 @@ authedClient.getCoinbaseAccounts((error, response, data) => {
   } else {
     var accounts = data;
     //find the active usd account
-    var usdAccount = accounts.filter(
+    var usdAccounts = accounts.filter(
       x => x.currency == "USD" && x.active == true
     );
-    if (usdAccount != null && usdAccount.length > 0) {
+    if (usdAccounts != null && usdAccounts.length > 0) {
       //for now just take the first one
-      console.log(`${usdAccount[0].name} USD account found`);
-      var usdBalance = usdAccount[0].balance;
+      this.usdAccount = usdAccounts[0];
+      console.log(`${this.usdAccount.name} USD account found`);
+      var usdBalance = this.usdAccount.balance;
       console.log(`${usdBalance} USD available`);
 
       //check to see if we have enough to preform trade
@@ -40,28 +70,27 @@ authedClient.getCoinbaseAccounts((error, response, data) => {
         //great - lets buy some BTC!
 
         // find the active btc wallet
-        var btcWallet = accounts.filter(
+        var btcWallets = accounts.filter(
           x => x.currency == "BTC" && x.active == true
         );
 
-        if(btcWallet != null && btcWallet.length > 0)
-        {
-            console.log(`${btcWallet[0].name} BTC account found`);
+        if (btcWallets != null && btcWallets.length > 0) {
+          //lets use the first one for now
+          this.btcAccount = btcWallets[0];
+          console.log(`${btcAccount.name} BTC account found`);
+        } else {
+          console.log("No active BTC account found");
+          return;
         }
-        else{
-            console.log("No active BTC account found");
-            return;
-        }
+      } else {
+        console.log("Not enough USD to buy BTC");
+        //we need to fund our account
+        fundAccount();
       }
-      else
-      {
-          console.log("Not enough USD to buy BTC");
-           //we need to fund our account
-           return;
-      }     
-    }
-    else {
-        console.log("No USD accounts found");
+    } else {
+      console.log("No USD accounts found");
     }
   }
 });
+
+
